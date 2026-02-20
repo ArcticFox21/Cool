6c9c7a9a8847e84d72f5e14a210a330bf7eaf416
ArcticFox21 <129649327+ArcticFox21@users.noreply.github.com>
2026-02-20 11:51:34 +0000

    Update cloud_version.lua

--- cloud_version.lua
+++ cloud_version.lua
@@ -1,1 +1,16 @@
-return "v1.0.0 - Cloud connection established!"
+return {
+    version = "v2.0.0 - The Storm Update",
+    
+    -- This is the actual code that will execute in the game
+    init = function()
+        -- 1. Print messages in the SAMP chat
+        sampAddChatMessage("{FF00FF}[Cloud] GitHub code successfully injected!", -1)
+        sampAddChatMessage("{FF00FF}[Cloud] Enjoy the stormy weather!", -1)
+        
+        -- 2. Print a huge GTA-style text on the middle of the screen for 3 seconds
+        printStringNow("~y~Cloud Script Loaded", 3000)
+        
+        -- 3. Force the local game weather to 8 (Stormy/Thunder)
+        forceWeatherNow(8)
+    end
+}
